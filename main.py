e425d73034ed9ab002ef95fa086148c8ace07faa
Bohdan Barannik <46631319+Travique@users.noreply.github.com>
2023-02-17 17:36:14 +0200

    syntax update
    fix some stupid decisions

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from selenium.webdriver.chrome.options import Options
 import vk_api
 import time 
 import random
+import os
 
 API_ID = "2685278"
 API_SECRET = "hHbJug59sKJie78wjrH8"
+TIME_SLEEP = 5.1
 
 async def take_screenshot(post_info_list):
     options = Options()
@@ -16,21 +18,21 @@ async def take_screenshot(post_info_list):
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
 
-    await asyncio.sleep(3)
+    await asyncio.sleep(TIME_SLEEP)
     
     browser = webdriver.Chrome(options=options)
     
     try:
-        ex = 0
-        while ex != len(post_info_list):
-            browser.get(post_info_list[ex])
+        for index, element in enumerate(post_info_list):
+            if index == len(post_info_list) + 1:
+                continue
+            
+            browser.get(post_info_list[index])
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            screenshot_path = f'post_comment_{random.randrange(0,100000)}_{ex}_{random.randint(0,1000000)}.png'
-            browser.save_screenshot(screenshot_path)
+            screenshot_path = f'post_comment_{random.randrange(0,100000)}_{index}_{random.randint(0,1000000)}.png'
+            browser.save_screenshot(f'{os.getcwd()}\Screens\{screenshot_path}')
             print(f'Screenshot saved to {screenshot_path}!')
-            ex += 1
-            
-            
+
         browser.quit()
        
             
@@ -43,9 +45,8 @@ async def post_comment(comment, post_id, owner_id):
     
     try:
         print(f'https://vk.com/wall-{owner_id}_{post_id} is waiting for comment.')
-        time.sleep(3)
         vk.wall.createComment(owner_id=-owner_id, post_id=post_id, message=comment)
-        time.sleep(3)
+        time.sleep(TIME_SLEEP)
         print(f'Comment {comment} posted successfully for https://vk.com/wall-{owner_id}_{post_id}.')
         
     except Exception as e:
@@ -73,17 +74,17 @@ async def main():
 
     match_info_list = []
     post_info_list = post_info.split('\n')
-    ex = 0
     
-    while ex != len(post_info_list):
+    for index, element in enumerate(post_info_list):
+        if index == len(post_info_list) + 1:
+            continue
         
-        match = re.search(r'vk\.com/wall-(\d+)_(\d+)', post_info_list[ex])
+        match = re.search(r'vk\.com/wall-(\d+)_(\d+)', post_info_list[index])
         
         if match:
             owner_id = int(match.group(1))
             post_id = int(match.group(2))
             match_info_list = match_info_list + [(owner_id, post_id)]
-            ex += 1
             
         else:
             print('Invalid post info!')
